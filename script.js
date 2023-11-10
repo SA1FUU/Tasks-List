aff1ef41c94c42efd6bcf67b5822637775cb9c2d
Mohd Saif <126072969+SA1FUU@users.noreply.github.com>
2023-11-10 22:47:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,264 +1,100 @@
-let mainContainer = document.querySelector('.main-container')
-let inputValue = document.querySelector('input')
-let addButton = document.querySelector('button')
-let listContainer = document.querySelector('.task-list-container')
-let userInfo = document.getElementById('users-information')
-let alertPopupBox = document.querySelector('.alert-popup-box')
-let universalAlert = document.querySelector('.alert-popup')
 
-// Function to Remove Snack Notification
+let container = document.querySelector("#container")
+let userContainer = document.querySelector(".user-container")
+let userClose = document.querySelector(".user-cross")
+let getAge = document.querySelector("#get-age")
+let userName = document.getElementById("name")
+let error = document.getElementById("error")
+let ageInfo = document.querySelector(".age-info")
+let ageClose = document.getElementById("age-close")
+let user = document.getElementById("user")
+let loader = document.querySelector(".loader")
 
-function removeUniversalAlert() {
-    setTimeout(() => {
-        alertPopupBox.style.visibility = "hidden"
-        universalAlert.innerText = ""
-    }, 3000);
-}
+function clickAgeBtn() {
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-// Event Listener Function to Add Task
-
-addButton.addEventListener('click', (e) => [
-    e.preventDefault(),
-    addTask()
-])
-
-// Validation for The Input Task 
-
-function addTask() {
-    if (inputValue.value === "") {
-        universalAlert.innerHTML = universalAlert.innerHTML + "Please Enter a Task "
-        alertPopupBox.style.visibility = "visible"
-        removeUniversalAlert()
+    let birthdate = new Date(document.getElementById("birthdate").value);
+    if (birthdate >= today) {
+        document.getElementById("date-error").innerText = "*Enter a Valid Date"
     }
     else {
-        let li = document.createElement("li")
-        li.innerText = `- ${inputValue.value}`
-        listContainer.appendChild(li)
-
-        let div = document.createElement('div')
-        div.classList.add("actions")
-        li.appendChild(div)
-
-        let i = document.createElement("i")
-        i.classList.add("fa-solid")
-        i.classList.add("fa-trash")
-
-        li.appendChild(i)
-
-        universalAlert.innerHTML = universalAlert.innerHTML + "One Task Added"
-        alertPopupBox.style.visibility = "visible"
-        removeUniversalAlert()
+        userContainer.style.visibility = "visible"
+        container.style.filter = "blur(15px)"
     }
-    saveData()
-    inputValue.value = ""
 }
 
-// Adding Event Listener on Task List
+userClose.addEventListener("click", () => {
+    userContainer.style.visibility = "hidden"
+    container.style.filter = "blur(0)"
+    error.textContent = ""
+    userName.value = ""
+    document.getElementById("date-error").innerText = ""
+})
 
-listContainer.addEventListener("click", (e) => {
-    if (e.target.tagName === "I") {
-        e.target.parentElement.remove()
-        saveData()
-        universalAlert.innerHTML = universalAlert.innerHTML + "One Task Deleted"
-        alertPopupBox.style.visibility = "visible"
-        removeUniversalAlert()
+getAge.addEventListener("click", () => {
+    if (userName.value === "") {
+        error.textContent = "*Please Enter Your Name"
     }
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked")
-        saveData()
-        universalAlert.innerHTML = universalAlert.innerHTML + "One Task Completed"
-        alertPopupBox.style.visibility = "visible"
-        removeUniversalAlert()
+    else {
+        calculateAge()
+        loader.classList.add("showloader")
+        userContainer.style.visibility = "hidden"
+        setTimeout(() => {
+            user.innerText = document.getElementById("name").value
+            loader.classList.remove("showloader")
+            ageInfo.style.visibility = "visible"
+            error.textContent = ""
+            userName.value = ""
+            document.getElementById("date-error").innerText = ""
+        }, 1200);
     }
-}, false)
+})
 
-// Function to Save Data on Local Storage
+ageClose.addEventListener("click", () => {
+    ageInfo.style.visibility = "hidden"
+    container.style.filter = "blur(0)"
+    user.innerText = ""
+    document.getElementById("birthdate").value = "00/00/0000"
+})
 
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML)
+
+function calculateAge() {
+    let birthdate = new Date(document.getElementById("birthdate").value);
+    let today = new Date();
+
+    // Calculate the difference in years, months, and days
+    let yearsDiff = today.getFullYear() - birthdate.getFullYear();
+    let monthsDiff = today.getMonth() - birthdate.getMonth();
+    let daysDiff = today.getDate() - birthdate.getDate();
+
+    // Adjust for negative months and days differences
+    if (daysDiff < 0) {
+        monthsDiff--;
+        daysDiff += 30; // Assuming an average of 30 days per month
+    }
+
+    if (monthsDiff < 0) {
+        yearsDiff--;
+        monthsDiff += 12;
+    }
+
+    let ageText = `${yearsDiff} Years, ${monthsDiff} Months And ${daysDiff} Days Old`;
+    document.getElementById("result").textContent = ageText;
 }
 
-function showData() {
-    listContainer.innerHTML = localStorage.getItem("data")
-}
-showData()
 
-
-// Add Function to Trigger Add Button on Enter Click
-
-inputValue.addEventListener("keypress", function (event) {
+document.querySelector("#birthdate").addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
-        addButton.click();
+        document.getElementById("cal-age").click();
+        document.querySelector("#name").focus()
     }
 });
 
-//  For Profile Thing
-
-let profileButton = document.querySelector('#profile-button')
-let profileDiv = document.querySelector('.profile-container')
-let submitButton = document.getElementById('save-btn')
-let errorMessage = document.querySelector('.error-message')
-
-profileButton.addEventListener("click", () => {
-    addProfileDiv()
-})
-
-function addProfileDiv() {
-    profileDiv.style.visibility = "visible"
-    mainContainer.style.filter = "blur(5px)"
-    notiInfoContainer.style.visibility = "hidden"
-}
-
-let closeProfileButton = document.querySelector("#close-profile-btn")
-
-closeProfileButton.addEventListener("click", () => {
-    profileDiv.style.visibility = "hidden",
-    mainContainer.style.filter = "blur(0)"
-    errorMessage.innerText = ""
-})
-
-
-submitButton.addEventListener("click", () => {
-    createProfile()
-})
-
-function createProfile() {
-    if (document.getElementById('name').value === "" && document.getElementById('gender').value === "none") {
-        errorMessage.innerText = "Fill the Required Details"
-        saveProfileData()
+document.querySelector("#name").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("get-age").click();
     }
-    else if (document.getElementById('name').value === "") {
-        errorMessage.innerText = "Enter the Username"
-        saveProfileData()
-    }
-    else if (document.getElementById('name').value.length > 10) {
-        errorMessage.innerText = "Username Should be Less than 10 Chars"
-        saveProfileData()
-    }
-    else if (document.getElementById('gender').value === "none") {
-        errorMessage.innerText = "Select Your Gender"
-        saveProfileData()
-    }
-    else if (document.getElementById('gender').value === "male") {
-        userInfo.innerText = `Hey, Mr. ${document.getElementById('name').value}`
-        profileDiv.style.visibility = "hidden",
-            mainContainer.style.filter = "blur(0)"
-        userInfo.style.backgroundImage = "linear-gradient(25deg, blueviolet, rgb(49, 186, 186))"
-        errorMessage.innerText = ""
-        saveProfileData()
-        universalAlert.innerHTML = universalAlert.innerHTML + "Profile Updated Successfully"
-        alertPopupBox.style.visibility = "visible"
-        removeUniversalAlert()
-    }
-    else if (document.getElementById('gender').value === "female") {
-        userInfo.innerText = `Hey, Ms. ${document.getElementById('name').value}`
-        profileDiv.style.visibility = "hidden",
-            mainContainer.style.filter = "blur(0)"
-        userInfo.style.backgroundImage = "linear-gradient(25deg, blueviolet, rgb(49, 186, 186))"
-        errorMessage.innerText = ""
-        saveProfileData()
-        universalAlert.innerHTML = universalAlert.innerHTML + "Profile Updated Successfully"
-        alertPopupBox.style.visibility = "visible"
-        removeUniversalAlert()
-    }
-    document.getElementById('name').value === ""
-    document.getElementById('gender').value === "none"
-    saveProfileData()
-}
-
-
-let deletProfileButton = document.getElementById("delete-btn")
-
-deletProfileButton.addEventListener("click", () => {
-    profileDiv.style.visibility = "hidden",
-        mainContainer.style.filter = "blur(0)"
-    userInfo.style.backgroundImage = "none"
-    userInfo.innerText = ""
-    errorMessage.innerText = ""
-    saveProfileData()
-    universalAlert.innerHTML = universalAlert.innerHTML + "Profile Deleted Successfully"
-    alertPopupBox.style.visibility = "visible"
-    removeUniversalAlert()
-})
-
-// Function to Save Profile Data
-
-function saveProfileData() {
-    localStorage.setItem("user-data", userInfo.innerText)
-}
-
-function showProfileData() {
-    userInfo.innerText = localStorage.getItem("user-data")
-}
-showProfileData()
-
-
-
-// Notification Scenes
-
-let closeAlertBoxBtn = document.getElementById('close-alertbox-btn')
-let mainAlertBtn = document.getElementById('main-alert-btn')
-let sendNotiButton = document.querySelector('#send-noti-btn')
-let notiInfoContainer = document.querySelector('.noti-info-container')
-let errorInNotification = document.getElementById('error-in-noti')
-
-
-mainAlertBtn.addEventListener("click", () => {
-    notiInfoContainer.style.visibility = "visible"
-    profileDiv.style.visibility = "hidden",
-        mainContainer.style.filter = "blur(10px)"
-
-        requestPerms()
-})
-
-closeAlertBoxBtn.addEventListener("click", () => {
-    notiInfoContainer.style.visibility = "hidden"
-    mainContainer.style.filter = "blur(0)"
-    errorInNotification.innerText = ""
-})
-
-
-sendNotiButton.addEventListener("click", () => {
-    let minutes = parseFloat(document.querySelector("#minutes-delay").value)
-    let exactDelay = minutes * 60 * 1000
-
-    // Check if the input is a valid number
-
-    if (isNaN(minutes) || minutes <= 0) {
-        errorInNotification.innerText = "Enter a Valid / Positive Time Duration"
-        return;
-    }
-    else {
-        setTimeout(() => {
-            sendNotification()
-        }, exactDelay);
-
-        universalAlert.innerHTML = universalAlert.innerHTML + `You Will be Notified After ${minutes} Minutes`
-        alertPopupBox.style.visibility = "visible"
-        removeUniversalAlert()
-    }
-    notiInfoContainer.style.visibility = "hidden"
-    mainContainer.style.filter = "blur(0)"
-    errorInNotification.innerText = ""
-
-})
-
-// Requesting Permission
-      
-function requestPerms() {
-    Notification.requestPermission()
-}
-
-// Notification Body if Access Granted
-
-function sendNotification() {
-        Notification.requestPermission().then(perm => {
-        if (perm === "granted") {
-            let notification = new Notification(`Hey, ${document.getElementById('name').value}`, {
-                body: "You Have a Pending Task",
-                icon: "https://tse1.mm.bing.net/th?id=OIP.tSp2HqX4psYo4zSvqnGHGgHaHa&pid=Api&P=0&h=180",
-            })
-        }
-    })
-}
+});
